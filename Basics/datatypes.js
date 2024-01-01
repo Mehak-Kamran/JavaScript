5512ea39bf13a449ffba94c3d5807d49d971cec7
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-01 12:27:33 +0000

    various datatyes

---
+++ Basics/datatypes.js
@@ -0,0 +1,27 @@
+//string
+let name="meak"
+//number
+let age=12
+//undefined
+let address;
+//object
+let salary=null
+
+//bool
+married=false
+
+//printing
+console.log(name,age,address,salary,married)
+
+//finding the type
+console.log(typeof(salary))
+console.log(typeof(married))
+
+// more over 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
