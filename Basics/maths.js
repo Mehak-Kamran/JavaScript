3209a4be5a956cb7c6dc5a69db337e6580d97e3d
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-03 18:33:30 +0000

    maths

---
+++ Basics/maths.js
@@ -0,0 +1,14 @@
+console.log(Math)
+console.log(Math.abs(-6))
+console.log(Math.round(23.876))
+console.log(Math.floor(23.876))
+console.log(Math.ceil(23.176))
+console.log(Math.max(2,7,5,9,6,3))
+console.log(Math.min(2,7,5,9,6,3))
+//most used
+console.log(Math.random())
+
+//if range is between 10 to 20 and not between 0to 1
+min=1
+max=10
+console.log(Math.floor(Math.random() * (max-min+1)+min))// just change the value of min max its a standard formula applicable for all
