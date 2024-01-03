9fa08d40d9192b5c9097519b4fc44e6a38cef835
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-03 18:01:56 +0000

    numbers

---
+++ Basics/numbers.js
@@ -0,0 +1,12 @@
+const score=10000000
+console.log(score)
+//or you can also create an object 
+const balance=new Number(1006.65767876)
+console.log(balance)
+//some methods of nos
+console.log(typeof(balance.toString()))
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(3));
+console.log(score.toLocaleString('en-IN'));
+
+
