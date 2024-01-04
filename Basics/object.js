a2edd23b6f6d0792b2e6b69092d0e2f91c882477
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-04 19:05:58 +0000

    object freeze

--- Basics/object.js
+++ Basics/object.js
@@ -15,3 +15,16 @@ const userjs={
 console.log(userjs.password);
 console.log(userjs['id']);
 console.log(userjs[pass])
+//change object
+userjs.password='ricki'
+console.log(userjs)
+
+//fuc in obj
+userjs.myf=function(){
+    console.log(`Hello my id is ${this.id}`)
+}
+console.log(userjs.myf())
+//obj freeze
+Object.freeze(userjs)
+userjs.name='ash'
+console.log(userjs.name);
