8d99bc3c323cf230227eca0d9a604a4662082daf
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-04 17:44:20 +0000

    ARRAYS

---
+++ Basics/array2.js
@@ -0,0 +1,8 @@
+let BTS=['Jimin','v','jk','rm','jin','hobi','suga']
+let BP=['LISA','ROSE','JISO','JENNI']
+
+// console.log(BTS,BP);
+// BTS.push(BP)
+// console.log(BTS)
+MARVEL=BTS.concat(BP)
+console.log(MARVEL)
