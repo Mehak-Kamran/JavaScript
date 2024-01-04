09d5c34fd387f18a2da817b44ea2cebd8c0e45ba
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-04 11:56:38 +0000

    strings and its method

--- Basics/strings.js
+++ Basics/strings.js
@@ -1,7 +1,34 @@
 //how to write a string
-let name='mehak'
-let subject='javascript'
-console.log(`hi my name is ${name} and I love ${subject}`)// use this symbol  `
+// let name='mehak'
+// let subject='javascript'
+// console.log(`hi my name is ${name} and I love ${subject}`)// use this symbol  `
+
+//methods simple string
+// let lastname= new String ('kamran')
+// console.log(lastname.toUpperCase())
+// console.log(lastname.length)
+// console.log(lastname.charAt(2))
+// console.log(lastname.indexOf('m'))
+// console.log(lastname.substring(0,3))// substring cannot take any neg value
+// console.log(lastname.slice(-5,4))//slice can take neg value
+//trim
+// let fname='       Siddiqui '
+// console.log(fname)  
+// console.log(fname.trim())
+
+//replace
+let url='mehakkamran56@gmail.com'
+console.log(url.replace('56','52'))
+
+//include
+// const market_list=['apple','dahi','pen']
+// console.log(typeof(market_list))// array  type is object and these method is applied on strings only , wheather array is string array 
+// console.log(market_list.includes('chips'))
+console.log(url.includes('get'))
+console.log(url.split('@'))
+
+
+
 
 
 
