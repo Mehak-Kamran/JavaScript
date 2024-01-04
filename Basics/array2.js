04edc4bf9bdba1c6bc24fcbcb3a47cf34579a659
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-04 17:53:00 +0000

    array

--- Basics/array2.js
+++ Basics/array2.js
@@ -1,8 +1,37 @@
 let BTS=['Jimin','v','jk','rm','jin','hobi','suga']
 let BP=['LISA','ROSE','JISO','JENNI']
 
-// console.log(BTS,BP);
-// BTS.push(BP)
-// console.log(BTS)
-MARVEL=BTS.concat(BP)
-console.log(MARVEL)
+console.log(BTS,BP);
+BTS.push(BP)
+console.log(BTS)
+console.log(BTS[7][2])
+// MARVEL=BTS.concat(BP)
+// console.log(MARVEL)
+
+//ALTERNATE OF CONCAT
+let score=[1,5,7,8,[3,6,[4,7,0]]]
+let scoreflat=score.flat(Infinity)
+console.log(scoreflat)
+
+//STORE ANY OTHER IN ARRAY 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
