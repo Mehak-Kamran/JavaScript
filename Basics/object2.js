5a3af6307e168e5109b55952abf0b0fda294bc12
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-05 13:18:06 +0000

    singletonobject

---
+++ Basics/object2.js
@@ -0,0 +1,19 @@
+//singleton object or constructor obj
+const score=new Object()
+score.id=1005
+score.name="ayesha"
+console.log(score);
+
+
+const Name={
+    id:1006,
+    username:{
+        fullname:{
+            firstname:"mak",
+            lastname:"kam"
+        }
+    }
+    
+}
+console.log(Name.username.fullname.firstname);
+
