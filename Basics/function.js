dd9511cc0b9604d0a4c31b6dca0f368d12fc5d22
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-05 17:46:11 +0000

    functions

---
+++ Basics/function.js
@@ -0,0 +1,40 @@
+ function addtwonum(num1,num2){
+    return num1+num2
+    }
+    console.log(addtwonum(2,1));
+
+
+
+// function loginmessage(username="someone"){
+//     return `${username} has logged in`
+// }
+
+// console.log(loginmessage('ash'))
+// console.log(`${username} has logged in`)
+
+
+//return
+function loginmessage(username){
+    if(!username){
+        console.log("please enter username");
+        return
+       
+ }
+
+    return `${username} has logged in`
+}
+
+console.log(loginmessage())
+
+//ifelse
+function loginmessage(username){
+    if(!username){
+        console.log("please enter username");
+       
+ }
+ else
+    console.log(`${username} has logged in`) 
+}
+console.log(loginmessage())
+
+//
