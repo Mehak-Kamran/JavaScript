ffcf4e19d635c41225af4e36806c51597e338fe1
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-07 17:08:54 +0000

    rev

--- revision/rev1.js
+++ revision/rev1.js
@@ -1,21 +1,34 @@
+// //rev
+// console.log("hello world")
+// //rev
+// const dob=13
+// var weight2022=30
+// let weight2024=55
+// //rev
+// let mobileno
+// const eid=256
+// let teachername="Amina"
+// //rev 
+// let list=[1,2,3,"hey",2.78]
+// let ob={
+//     name:"mak",
+//     idcard:3456
+// }
+// function fun(){
+//     console.log(ob,list,teachername,eid,mobileno,weight2024,weight2022,dob)
+// }
+//  fun()
 //rev
-console.log("hello world")
-//rev
-const dob=13
-var weight2022=30
-let weight2024=55
-//rev
-let mobileno
-const eid=256
-let teachername="Amina"
-//rev 
-let list=[1,2,3,"hey",2.78]
-let ob={
-    name:"mak",
-    idcard:3456
+BTS={
+    name:'BTS',
+    genre:"HipHop",
+    members:["Rm","Jin","Suga","Jhope","Jimin","Jk","V"],
+    avgage:26
 }
-function fun(){
-    console.log(ob,list,teachername,eid,mobileno,weight2024,weight2022,dob)
+BP={
+    name:'BP',
+    genre:"HipHop",
+    members:["Lisa","Jenni","Jiso","Rose"],
+    avgage:23
 }
-fun()
-//rev
+console.log(BP.members);
