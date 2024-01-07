4f3b48040b41b9090105b1734988dedcb37efaaa
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-07 17:00:59 +0000

    revision

---
+++ revision/rev1.js
@@ -0,0 +1,21 @@
+//rev
+console.log("hello world")
+//rev
+const dob=13
+var weight2022=30
+let weight2024=55
+//rev
+let mobileno
+const eid=256
+let teachername="Amina"
+//rev 
+let list=[1,2,3,"hey",2.78]
+let ob={
+    name:"mak",
+    idcard:3456
+}
+function fun(){
+    console.log(ob,list,teachername,eid,mobileno,weight2024,weight2022,dob)
+}
+fun()
+//rev
