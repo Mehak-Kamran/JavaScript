bae3901ef6d00b8225c70ab6a258ae675b227da9
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-10 16:51:25 +0000

    hoisting

--- Basics/scope.js
+++ Basics/scope.js
@@ -10,3 +10,16 @@ console.log(a);
 //console.log(b);//error
 //console.log(c);//error
 console.log(a);
+
+//nested if scoping 
+//hoisting
+add1(2)
+function add1(no){
+   return no+1
+}
+//add1(2)
+add2(2)//error , that is called hoisting
+let add2=function (no){
+   return no+1
+}
+//add2(2)
