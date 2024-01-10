b01c8c98fc7f7512cdedd7389f0d595869045733
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-10 18:47:28 +0000

    arrow funct this

--- Basics/arrfun.js
+++ Basics/arrfun.js
@@ -17,10 +17,28 @@ const eg=(num1,num2)=>(num1+num2)//implicit arrow function
 //console.log(eg(1,2))
 
 //explicit arrow func
+//console.log(min(1,1))
  min=(num1,num2)=>{
     return num1-num2
 }
-//console.log(min(1,1))
+console.log(min(1,1))
+
+//In normal  func hoisting can be done but in arrow func you can not do hoisting
+
+//normal func
+function paint(){
+    sketch="duck"
+    console.log(this.sketch);
+}
+paint()
+
+//arrow func
+calli=()=>{
+    sketch="picasso"
+    console.log(this.sketch)
+}
+calli()
+//this cannot be used in arrow func
 
 
 
