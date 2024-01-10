a47d75db18d883fa0f6d668115777e849bffa410
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-10 18:38:32 +0000

    arr fun

--- Basics/arrfun.js
+++ Basics/arrfun.js
@@ -1,26 +1,27 @@
 //this refers to the current scope
-//it only refers in object not in func or arrow func
+ 
 
-// BTS={
-//     name:"Jimin",
-//     track:"like crazy",
-//     welcome:function(){
-//         console.log(this.name);
+ BTS={
+    name:"Jimin",
+    track:"like crazy",
+    welcome:function(){
+       console.log(this.name);
 
-//     }
+    }
     
-// }
+ }
 // BTS.welcome()
 
 //arrow function
 const eg=(num1,num2)=>(num1+num2)//implicit arrow function
-console.log(eg(1,2))
+//console.log(eg(1,2))
 
 //explicit arrow func
  min=(num1,num2)=>{
     return num1-num2
 }
-console.log(min(1,1))
+//console.log(min(1,1))
+
 
 
 
