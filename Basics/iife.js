20a44033e6c954b0c5aa1fbb884631e1539c718a
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-15 18:32:44 +0000

    iife

--- Basics/iife.js
+++ Basics/iife.js
@@ -1,11 +1,20 @@
-//To get rid of global pollution we use IMEDIATELY INVOKE FUNCTION EXPRESSION
-connect=function(){
-    console.log("Database Connected")
-}
-//connect()
+// //To get rid of global pollution we use IMEDIATELY INVOKE FUNCTION EXPRESSION
+// connect=function(){
+//     console.log("Database Connected")
+// }
+// //connect()
 
-//now we will make an iife 
-connected=(function(){
-    console.log("Database Connected");
-})
-()
+// //now we will make an iife 
+// connected=(function(){
+//     console.log("Database Connected");
+// })
+// //()
+
+//you can write like this 
+(function print(my){
+    console.log("hey"+my+"mehak");
+}
+)("  meaww  ");
+
+
+
