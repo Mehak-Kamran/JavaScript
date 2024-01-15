fcdae0f9abaf68af6ad50cb05ecba8b854c4bf50
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-15 17:44:57 +0000

    iife

---
+++ Basics/iife.js
@@ -0,0 +1,7 @@
+//To get rid of global pollution we use IMEDIATELY INVOKE FUNCTION EXPRESSION
+connect=function(){
+    console.log("Database Connected")
+}
+//connect()
+
+//now we will make an iife 
