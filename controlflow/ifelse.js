df7becc2d34b80dd6df49e62e10973940d13ee26
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-16 18:37:39 +0000

    nestedifelse

--- controlflow/ifelse.js
+++ controlflow/ifelse.js
@@ -1,10 +1,24 @@
 let age =21
 if(age<=20)
  {
-    console.log("young");
+    //console.log("young");
  }
  else{
-    console.log("old");
+    //console.log("old");
  }
+ //nested ifelse
+let city="islamaabd"
+ if(city==="karachi") 
+ {
+   console.log("city matched");
+ }
+ else if(city==="lahore"){
+          console.log("city is metropolitan");
+          }
+      else if(city==="khairpur"){
+              console.log("city id diffrent")
+            }
+            else console.log("no match try again");
+
 
  
