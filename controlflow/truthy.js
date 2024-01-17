66e4ace8470cfff7bd7c4747299507b7a2e371b3
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-17 17:43:01 +0000

    null coalscing operator

---
+++ controlflow/truthy.js
@@ -0,0 +1,19 @@
+useremail=undefined
+if(useremail){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+//truthy falsy
+//falsy=== false,0,'',null,undefined,NAN,-0,BIGINT 0N
+//TRUTHY ===ALL remaing are truthy values including "false","0" " ", [], {}, function(){}
+
+
+// null coalscing operator
+no=null ?? 4
+console.log(no);
+no=10 ?? 4
+console.log(no);// used when database is involved
+
+//ternary operator
