0096480ca8d5eeb3409a22f84bb054d26d6a396d
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-17 15:19:02 +0000

    nested switch

--- controlflow/switch.js
+++ controlflow/switch.js
@@ -17,5 +17,46 @@ switch(name){
         break;
 }
 
+function verify(city,country){
+    switch (country) {
+        case "Pakistan":
+            switch (city) {
+                case "Karachi":
+                    console.log("country is pak and city is karachi");
+                    break;
+                case "Lahore":
+                    console.log("country is pak and city is LAHORE");
+                    break;
+            }
+            break;
+    
+        case "USA":
+            switch (city) {
+                case "NYC":
+                    console.log("country is USA and city is NYC");
+                    break;
+                case "LA":
+                    console.log("country is USA and city is LA");
+                    break;
+            }
+            break;
+        case "KOREA":
+            switch (city) {
+                case "SEOUL":
+                    console.log("country is USA and city is SEOUL");
+                    break;
+                case "BUSAN":
+                    console.log("country is USA and city is BUSAN");
+                    break;
+            }
+            break;
+        default:
+            console.log("no country city matched");
+            break;
+    }
+
+}
+
+
 
     
