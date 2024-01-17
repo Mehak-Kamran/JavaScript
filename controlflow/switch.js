9a1a920005e8909f4db3c08d27a5dafe639821a9
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-17 15:08:16 +0000

    switch

---
+++ controlflow/switch.js
@@ -0,0 +1,21 @@
+let name="jimin"
+switch(name){
+    case "peter":
+        console.log("true");
+        break;
+    case "henry":
+        console.log("true");
+        break;
+    case "Josh":
+        console.log("true");
+        break;
+    case "jimin":
+        console.log("yes");
+        break;
+    default:
+        console.log("not matched");
+        break;
+}
+
+
+    
