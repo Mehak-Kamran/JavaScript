c000ab6c1d587103033c8a36f5e30d552d5578f2
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-18 12:04:48 +0000

    while loop

---
+++ loop/two.js
@@ -0,0 +1,15 @@
+//while loop
+let i=0
+while(i<10){
+//console.log("hey")
+i++
+}
+//array in while
+myarray=["alif","jimin","baluga"]
+let j=0
+while(j<myarray.length){
+    console.log(myarray[j]);
+    j++
+}
+
+
