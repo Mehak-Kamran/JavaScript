577e1268d1cbdcf2004feb48f99a94d88756f198
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-19 17:17:39 +0000

    map

--- loop/four.js
+++ loop/four.js
@@ -20,9 +20,26 @@ let kpop=[
 
 
 let jay=kpop.filter( (item)=>item.favsong=="boy with luv")
-console.log(jay);
+//console.log(jay);
 
 
 
-//noofmemfilter=kpop.filter((item)=>(item.members>=5))
+noofmemfilter=kpop.filter((item)=>(item.members>=5))
 //console.log(noofmemfilter);
+
+//map
+let an=[1,2,3,4,5,6]
+va=an.map((item)=>item+1)
+console.log(va);
+//chaining in map
+ga=an.map((item)=>item+1) .map((item)=>item*0).filter((item)=>item==0)
+console.log(ga);
+
+
+
+
+
+
+
+
+
