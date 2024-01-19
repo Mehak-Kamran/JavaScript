0dbf3e6fcb05cfb7a2a75e6cf7a17ed14be11440
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-19 17:28:51 +0000

    reduce

--- loop/four.js
+++ loop/four.js
@@ -22,18 +22,39 @@ let kpop=[
 let jay=kpop.filter( (item)=>item.favsong=="boy with luv")
 //console.log(jay);
 
-
-
 noofmemfilter=kpop.filter((item)=>(item.members>=5))
 //console.log(noofmemfilter);
+
+
+
 
 //map
 let an=[1,2,3,4,5,6]
 va=an.map((item)=>item+1)
-console.log(va);
+//console.log(va);
 //chaining in map
-ga=an.map((item)=>item+1) .map((item)=>item*0).filter((item)=>item==0)
-console.log(ga);
+ga=an.map((item)=>item+1) .map((item)=>item+1).filter((item)=>item<=6)
+//console.log(ga);
+
+
+//reduce
+let shoppingcart=[
+    {
+        accessory:"clothes",
+        price:200
+    },
+    {
+        accessory:"jewellery",
+        price:100
+    },
+    {
+        accessory:"cosmetics",
+        price:500
+    }
+]
+
+moneytopay=shoppingcart.reduce( (acc,item)=>(acc+item.price),150 )
+console.log(moneytopay);
 
 
 
