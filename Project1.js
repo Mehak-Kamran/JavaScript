17833a07b1b37c6acb65dbff25419c10baf3f3cd
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-27 12:14:53 +0000

    color switcher

---
+++ Project1.js
@@ -0,0 +1,28 @@
+//color switcher 
+
+buttons = document.querySelectorAll('.button');
+body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (event) {
+    console.log(event);
+    console.log(event.target);
+
+    if (event.target.id === 'grey') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'white') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'blue') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'yellow') {
+      body.style.backgroundColor = event.target.id;
+    }
+    if (event.target.id === 'purple') {
+      body.style.backgroundColor = event.target.id;
+    }
+  });
+});
