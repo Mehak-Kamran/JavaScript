0532f4a18760b11bb5dd4fb513f3ea581d907002
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-01-31 08:41:53 +0000

    CHANGES

---
+++ Project/script.js
@@ -0,0 +1,22 @@
+h1=document.querySelector(".myclass")
+h1.innerText='Hello'
+h1.style.color='Pink'
+
+f=h1.setAttribute("class","newclass")
+
+
+//h1.classList.add("newclass")
+
+
+button = document.createElement("button")
+button.innerText="Click me"
+button.style.backgroundColor="pink"
+div=document.querySelector("body")
+div.append(button)
+
+//alert("Hey")
+
+
+
+
+
