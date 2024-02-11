d01c9ded5fc2c38d4de1b22b2718705bba7df244
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-02-11 08:24:23 +0000

    random cat facts

--- API/script.js
+++ API/script.js
@@ -1,8 +1,14 @@
 URL= "https://cat-fact.herokuapp.com/facts"
+getfact=document.querySelector(".printfact")
+let button=document.querySelector(".btn")
+
 
 async function facts (){
 
     let response=await fetch(URL) 
     let data = await response.json()
     console.log(data[3].text)
+   getfact.innerText=(data[3].text)
 }
+
+button.addEventListener("click",facts)
