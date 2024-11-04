9894f8d1a1f38cdf02b6b3681251d5d0d6cb1dc3
Mehak Kamran <130895685+Mehak-Kamran@users.noreply.github.com>
2024-11-04 17:32:08 +0000

    var vs const

---
+++ revision/rev3.js
@@ -0,0 +1,17 @@
+
+const check=()=>{
+    for (let i=0;i<5;i++){
+        console.log(i)
+    }
+    console.log("hello", i)
+}
+check()
+
+//functiobnally scoped var
+// const check=()=>{
+//     for (var i=0;i<5;i++){
+//         console.log(i)
+//     }
+//     console.log("hello", i)
+// }
+// check()
